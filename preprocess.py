6e906fc2256ce3961108429f8c9ae471f0e4c050
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2024-03-06 14:11:49 -0500

    updated to hierarchical lstm model and updated preprocessing methods

--- preprocess.py
+++ preprocess.py
@@ -1,97 +1,122 @@
-from music21 import converter, note, chord
+import music21
+from music21 import converter, note, instrument, harmony, midi, roman, key, stream
+from music21 import environment, configure
+import music21.chord as chord_module
+import music21.note as note_module
 import glob
-import numpy as np
 import json
 
-def midi_to_notes(file_path, time_step=0.25, destination_path='output.txt'):
-    """_summary_
+def extract_notes_with_music21(file_path):
+    # Load the MIDI file
+    midi = converter.parse(file_path)
 
-    Args:
-        file_path (_str_): Return a compilation of notes from midi files in file_path
-    """
+    original_key = midi.analyze('key')
+    transposed_key = key.Key('C')
+    interval = music21.interval.Interval(original_key.tonic, transposed_key.tonic)
+    transposed_stream = midi.transpose(interval)
+
+    # Dictionary to store notes by their offset
+    all_chords = []
     all_notes = []
-    for file in glob.glob(file_path):
-        notes = []
-        try:
-            midi = converter.parse(file)
-            # Proceed with your code to flatten the score and extract notes...
-        except Exception as e:
-            print(f"Error parsing {file}: {e}")
+    for element in midi.recurse():
+        if any(cls in ['Unpitched', 'PercussionChord'] for cls in element.classes):
+            rest = note_module.Rest()
+            rest.duration = element.duration
+            if element.activeSite:
+                element.activeSite.replace(element, rest)
 
-        # Flatten the score to a single stream while keeping offsets
-        notes_to_parse = midi.flat.notesAndRests
-
-        for element in notes_to_parse:
-            steps = int(element.duration.quarterLength / time_step)
-            if isinstance(element, note.Note):
-                symbol = element.pitch.midi
-            elif isinstance(element, chord.Chord):
+    chords = transposed_stream.chordify()
+    for measure in chords.getElementsByClass('Measure'):
+        measure_lst = []
+        note_durations = {}
+        for element in measure.notes:
+            if isinstance(element, chord_module.Chord):
                 symbol = '.'.join(str(n.midi) for n in element.pitches)
+                for note in element.pitches:
+                    note_name = note.nameWithOctave
+                    note_durations[note_name] = note_durations.get(note_name, 0) + element.duration.quarterLength
             elif isinstance(element, note.Rest):
                 symbol = "r"
             else: 
                 continue
-            for step in range(steps):
-                if step == 0:
-                    notes.append(str(symbol))
-                else:
-                    notes.append("_")
+            measure_lst.append(symbol)
 
-        all_notes.extend(notes + ["<end>"])
-        # all_notes.append(" ".join(notes) + " " + "<end>")
+        if note_durations:
+            sorted_notes = sorted(note_durations, key=note_durations.get, reverse=True)[:4]
+            freq = chord_module.Chord(sorted_notes)
+            rn = roman.romanNumeralFromChord(freq, transposed_key)
+            simplified_name = simplify_roman_name(rn)
+        
+        else:
+            simplified_name = 'rest_or_no_chord'
 
-    with open(destination_path, "w") as file:
-        file.write(" ".join(all_notes))
-    return all_notes
+        # Append measure list to the correct key in the dictionary
+        all_chords.append(simplified_name)
+        all_notes.append(measure_lst)
+    return all_chords, all_notes
 
-def preprocess_notes(single_file_dataset="output.txt", mappings_file="mappings.json", sequence_length=50):
-    # n_vocab = len(set(notes)) # 326 unique notes and chords
-    # pitchnames = set(item for item in notes)
+def simplify_roman_name(roman_numeral):
+    """
+    Simplify roman numeral chord names.
+    """
+    simplified_name = roman_numeral.figure
+    if roman_numeral.inversion() == 0:
+        return simplified_name
+    else:
+        # Add inversion to the chord name
+        return f"{simplified_name}/{roman_numeral.bass().name}"
+    
 
-    # note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-    with open(single_file_dataset, "r") as fp:
-        all_notes = fp.read()
-    encoded_notes = convert_to_int(all_notes, mappings_file)
-
+def preprocess_sequences(items, sequence_length, mappings_file):
+#     with open(single_file_dataset, "r") as fp:
+#         all_notes = fp.read()        
+    encoded_notes = convert_to_int(items, mappings_file)
     network_input = []
     network_output = []
-    n_vocab = len(set(encoded_notes))
-
-    for i in range(0, len(encoded_notes) - sequence_length, 1):
-        network_input.append(encoded_notes[i:i+sequence_length])
-        network_output.append(encoded_notes[i+sequence_length])
-
-    n_patterns = len(network_input)
-    network_input = np.reshape(network_input, (n_patterns, sequence_length))
-    network_input = network_input / float(n_vocab)
-
-    network_output = np.array(network_output)
-    return n_vocab, network_input, network_output
-
-def create_mappings(notes, file_path):
-    mappings = {}
+    for i in range(len(items) - sequence_length):
+        sequence_in = items[i:i + sequence_length]
+        sequence_out = items[i + sequence_length]
+        network_input.append(sequence_in)
+        network_output.append(sequence_out)
+    n_vocab = len(set(items))
+    return np.array(network_input), np.array(network_output), n_vocab
     
-    pitchnames = set(item for item in notes)
-    for number, note in enumerate(pitchnames):
-        mappings[note] = number
+def create_mappings(items, file_path):
+    unique_items = {'_'.join(item) for item in items}
+    mappings = {item: number for number, item in enumerate(unique_items)}
     with open(file_path, "w") as file:
         json.dump(mappings, file)
+    return mappings
 
-def convert_to_int(notes, mapping_file):
+def convert_to_int(items, mapping_file):
     int_notes = []
-
     with open(mapping_file, "r") as fp:
         mappings = json.load(fp)
-
-    for note in notes:
-        int_notes.append(mappings[note])
+    for item in items:
+        item_str = '_'.join(item) if isinstance(item, list) or isinstance(item, str) else item
+        int_notes.append(mappings.get(item_str, -1))
     return int_notes
 
+
 def main():
-    file_path = "../datasets/midi_songs/*.mid"
-    output_path = "output.txt"
-    notes = midi_to_notes(file_path, 0.25, output_path)
-    create_mappings(notes, "mappings.json")
+    file_path = "../datasets/classical_music_dataset/mozart/mz_311_1.mid"
+    midi_files = glob.glob(file_path)
+    all_files_chords = []
+    all_files_notes = []
+    for file in midi_files:
+        all_chords, all_notes = extract_notes_with_music21(file)
+        all_files_chords.extend(all_chords)
+        all_files_notes.extend(all_notes)
+
+    note_mapping_path = "note_mappings.txt"
+    chord_mapping_path = "chord_mappings.txt"
+    # Convert notes and chords to integer sequences
+    notes_int = convert_to_int(all_files_notes, note_mapping_path)
+    chords_int = convert_to_int(all_files_chords, chord_mapping_path)
+
+    # Prepare input and output sequences
+    network_input_notes, network_output_notes, n_vocab_notes = preprocess_sequences(notes_int, sequence_length, note_mapping_path)
+    network_input_chords, network_output_chords, n_vocab_chords = preprocess_sequences(chords_int, sequence_length, chord_mapping_path)
 
 if __name__ == "__main__":
     main()
--- train_model.py
+++ train_model.py
@@ -1,32 +1,54 @@
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM, Activation, Embedding
+
 from preprocess import preprocess_notes
+
+
+import tensorflow as tf
+import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, LSTM, Activation, Embedding, Concatenate, Input
+from keras.layers import BatchNormalization as BatchNorm
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 
-EPOCHS = 200
+
+sequence_length = 16
+EPOCHS = 100
 BATCH_SIZE = 64
-LEARNING_RATE = 0.001
+learning_rate = 0.001
+sequence_length = 16
+note_embedding_dim, chord_embedding_dim = 100, 100
+lstm_units = 256
+dropout_rate = 0.35
 MODEL_PATH = "model.h5"
+output_path = "output.mid"
 
-def generate_model(sequence_length, n_vocab, embedding_dim=100, dropout=0.35, learning_rate=LEARNING_RATE):
-    model = Sequential()
-    model.add(Embedding(n_vocab, embedding_dim, input_length=sequence_length))
-    model.add(LSTM(256, return_sequences=True))
-    model.add(Dropout(dropout))
-    model.add(LSTM(512, return_sequences=True))
-    model.add(Dropout(dropout))
-    model.add(LSTM(256))
-    model.add(Dense(256))
-    model.add(Dropout(dropout))
-    model.add(Dense(n_vocab))
-    model.add(Activation('softmax'))
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(learning_rate=learning_rate))
+def hierarchical_lstm_model(sequence_length, n_vocab_notes, n_vocab_chords, note_embedding_dim, chord_embedding_dim, lstm_units, dropout_rate, learning_rate):
 
-    model.summary()
+    note_input = Input(shape=(sequence_length,), name='note_input')
+    note_embedding = Embedding(input_dim=n_vocab_notes + 1, output_dim=note_embedding_dim, input_length=sequence_length, name='note_embedding')(note_input)
+    note_lstm = LSTM(lstm_units, return_sequences=True, name='note_lstm')(note_embedding)
+    note_lstm_dropout = Dropout(dropout_rate, name='note_dropout')(note_lstm)
+
+    chord_input = Input(shape=(sequence_length,), name='chord_input')
+    chord_embedding = Embedding(input_dim=n_vocab_chords + 1, output_dim=chord_embedding_dim, input_length=sequence_length, name='chord_embedding')(chord_input)
+    chord_lstm = LSTM(lstm_units, return_sequences=True, name='chord_lstm')(chord_embedding)
+    chord_lstm_dropout = Dropout(dropout_rate, name='chord_dropout')(chord_lstm)
+
+    combined = Concatenate(name='concatenate')([note_lstm_dropout, chord_lstm_dropout])
+    combined_lstm = LSTM(lstm_units, name='combined_lstm')(combined)
+    combined_dropout = Dropout(dropout_rate, name='combined_dropout')(combined_lstm)
+    
+    chord_output = Dense(n_vocab_chords + 1, activation='softmax', name='chord_output')(combined_dropout)
+    note_output = Dense(n_vocab_notes + 1, activation='softmax', name='note_output')(combined_dropout)
+    
+    model = Model(inputs=[note_input, chord_input], outputs=[note_output, chord_output])
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss={'note_output': 'sparse_categorical_crossentropy', 'chord_output': 'sparse_categorical_crossentropy'},
+                  loss_weights={'note_output': 1.0, 'chord_output': 1.0})
 
     return model
 
-def train_model(model, network_input, network_output, epochs=200, 
+def train_model(model, network_input, network_output, epochs=200,
                 batch_size=64):
     """
     Train the model with the given number of epochs and batch size and
@@ -39,19 +61,19 @@ def train_model(model, network_input, network_output, epochs=200,
         epochs (int, optional): _description_. Defaults to 200.
         batch_size (int, optional): _description_. Defaults to 64.
         filepath (str, optional): _description_. Defaults to 'weights-{epoch:02d}-{loss:.4f}.hdf5'.
-        
+
     Returns:
         _type_: _description_
     """
 
-    model.fit(network_input, network_output, epochs=epochs, 
+    model.fit(network_input, network_output, epochs=epochs,
               batch_size=batch_size)
 
-def train(sequence_length, learning_rate=LEARNING_RATE):
+def train():
+    network_input = [network_input_notes, network_input_chords]
+    network_output = [network_output_notes, network_output_chords]
 
-    n_vocab, network_input, network_output = preprocess_notes(sequence_length=sequence_length)
-
-    model = generate_model(sequence_length, n_vocab)
+    model = hierarchical_lstm_model(sequence_length, n_vocab_notes, n_vocab_chords, note_embedding_dim, chord_embedding_dim, lstm_units, dropout_rate, learning_rate)
 
     train_model(model, network_input, network_output, epochs=EPOCHS, batch_size=BATCH_SIZE)
 
